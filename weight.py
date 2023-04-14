905dabf78052320d07aaf51bd01a9f3d9c3ad8dd
David-Semilogo <davidsemilogo1231@gmail.com>
2023-04-14 14:26:18 +0100

    Updating weight.py file

--- weight.py
+++ weight.py
@@ -4,8 +4,9 @@ weight = input("Enter your weight in kilogram: ")
 weight = int(weight)
 
 
-weight_in_pound=weight*2.2
+weight_in_pound = weight*2.2
 
 
 print("Your weight in pounds is {:0.2f} pounds".format(weight_in_pound))
 
+print("Ademola")
