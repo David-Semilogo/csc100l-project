44142fd446a84e1907ce1dd6308c52f6e2f4d98b
David-Semilogo <davidsemilogo1231@gmail.com>
2023-04-14 14:27:10 +0100

    Revert "Updating weight.py file"
    This reverts commit 905dabf78052320d07aaf51bd01a9f3d9c3ad8dd.

--- weight.py
+++ weight.py
@@ -4,9 +4,8 @@ weight = input("Enter your weight in kilogram: ")
 weight = int(weight)
 
 
-weight_in_pound = weight*2.2
+weight_in_pound=weight*2.2
 
 
 print("Your weight in pounds is {:0.2f} pounds".format(weight_in_pound))
 
-print("Ademola")
